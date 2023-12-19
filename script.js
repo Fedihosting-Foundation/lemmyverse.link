a80a290eeb35977ee597255d8d8b1d24787e10af
Pdzly <34943569+Pdzly@users.noreply.github.com>
2023-12-19 21:23:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -9,39 +9,73 @@
 // @grant        GM_setClipboard
 // @grant        GM_document
 // @grant        GM_addStyle
+// @grant        GM_getState
 // @run-at       document-start
 
 // ==/UserScript==
 
 const is_lemmy_page = () => window.location.pathname.startsWith('/u/') && window.location.pathname.split('/').length === 3 && document.querySelector('a[href="https://join-lemmy.org"') !== null;
 
+const add_button = () => {
+
+    if (document.querySelector('a.person-listing') === null || document.querySelector('#lemmyverse-copy-button') !== null) {
+        return;
+    }
+    // Add a button to copy the user's name into this pattern https://lemmyverse.link/u/username
+    let user_name = window.location.pathname.split('/')[2];
+    const button = document.createElement('button');
+
+    if (!user_name.includes('@')) {
+        user_name = user_name + '@' + window.location.hostname;
+    }
+
+    button.innerText = 'Copy Lemmyverse link';
+    button.addEventListener('click', () => {
+        navigator.clipboard.writeText(`https://lemmyverse.link/u/${user_name}`).then(function () {
+            console.log('[Lemmyverse] Copying to clipboard was successful!');
+        }, function (err) {
+            console.error('[Lemmyverse] Could not copy text: ', err);
+        });
+
+    }
+    );
+    button.className = "d-flex align-self-start btn btn-secondary me-2"
+
+    button.id = 'lemmyverse-copy-button';
+
+    document.querySelector('a.person-listing').parentElement.parentElement.parentElement.parentElement.appendChild(button);
+}
+
 (function () {
     'use strict';
-    document.addEventListener("DOMContentLoaded", function () {
-        console.log(document.querySelector('a[href="https://join-lemmy.org"') !== null);
+    document.addEventListener('DOMContentLoaded', () => {
+        if (is_lemmy_page()) {
+            add_button();
+        }
+        
+        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+        if( MutationObserver ){
+            // define a new observer
+            var mutationObserver = new MutationObserver(() => {
+                console.log('[Lemmyverse] Checking...');
+                if (is_lemmy_page()) {
+                    add_button();
+                }
+            })
+      
+            // have the observer observe for changes in children
+            mutationObserver.observe( document.body, { childList:true, subtree:true })
+            return mutationObserver
+          }
+        
+    });
+  
 
-        console.log(window.location.pathname.startsWith('/u/') && window.location.pathname.split('/').length === 3);
+    window.addEventListener("popstate", function () {
+        console.log('[Lemmyverse] Ready');
         // Check if  we are on a lemmy page
         if (is_lemmy_page()) {
-            // Add a button to copy the user's name into this pattern https://lemmyverse.link/u/username
-            const user_name = window.location.pathname.split('/')[2];
-            const button = document.createElement('button');
-
-            button.innerText = 'Copy Lemmyverse link';
-            button.addEventListener('click', () => {
-                navigator.clipboard.writeText(`https://lemmyverse.link/u/${user_name}`).then(function () {
-                    console.log('[Lemmyverse] Copying to clipboard was successful!');
-                }, function (err) {
-                    console.error('[Lemmyverse] Could not copy text: ', err);
-                });
-
-            }
-            );
-            button.className = "btn btn-secondary me-2"
-            button.style = "margin-left: auto; margin-top: 10px"
-
-            document.querySelector('a.person-listing').parentElement.parentElement.parentElement.parentElement.appendChild(button);
+            add_button()
         }
     });
-
 })();
